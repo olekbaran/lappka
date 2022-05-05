8d1fe63c085c8c3b2fc622b3afa04484446cc16d
olek-arsee <olboygg@gmail.com>
2022-05-05 12:57:49 +0200

    feat: pwa

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,10 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('serviceWorker.js');
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
